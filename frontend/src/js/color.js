adac1f764767253a23efb461ec502c029637beef
ShujiOba <shuji365630@gmail.com>
2019-10-26 11:39:45 +0900

    :art: Add color constant

---
+++ frontend/src/js/color.js
@@ -0,0 +1,15 @@
+export const color = str => colors[str] || '#ffffff';
+
+const colors = {
+  aqua: '#4dd0e1',
+  lime: '#cddc39',
+  yellow: '#ffc107',
+  purple: '#ab47bc',
+  pink: '#e91e63',
+  orange: '#ff9800',
+  green: '#4caf50',
+  blue: '#3f51b5',
+  red: '#f44336',
+  black: '#3c3c3c',
+  white: '#ffffff'
+};
