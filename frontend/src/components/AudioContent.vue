585e7bdde7054aa958401600a53fa3e85ab06063
schktjm <si2.o.mjt@gmail.com>
2019-10-27 00:57:01 +0900

    feature audio item

---
+++ frontend/src/components/AudioContent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="audio-content-wrapper">
+    <music-icon class="audio-icon-style"/>
+    <audio class="audio-style" :src="src" controls></audio>
+  </div>
+</template>
+
+<script>
+import MusicIcon from 'vue-material-design-icons/Music.vue';
+
+export default {
+  components: { MusicIcon },
+  props: {
+    src: String,
+  },
+};
+</script>
+
+<style scoped>
+.audio-content-wrapper {
+    position: relative;
+    width: 60vh;
+    height: 60vh;
+    background-color: var(--color-yellow);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.audio-icon-style {
+    font-size: 300px;
+}
+
+.audio-style {
+    position: absolute;
+    bottom: 0;
+}
+</style>
