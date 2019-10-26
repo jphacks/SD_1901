8ad5c0578addf3d692757de5e92a357c655d0859
schktjm <si2.o.mjt@gmail.com>
2019-10-26 17:05:35 +0900

    feature file item

---
+++ frontend/src/components/FileContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="file-content-flame">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: String,
+  },
+};
+</script>
+
+<style scoped>
+.file-content-flame {
+    border: 1px solid black;
+    background-color: #dfdfdf;
+}
+
+.file-content-flame img {
+    object-fit: contain;
+}
+</style>
