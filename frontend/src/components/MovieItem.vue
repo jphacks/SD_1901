c1c02f54753e4a2482acd72227f0bb8a9f5101ac
schktjm <si2.o.mjt@gmail.com>
2019-10-26 23:13:33 +0900

    feature movie item

---
+++ frontend/src/components/MovieItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <basic-button
+    color="blue"
+    :image="image"
+    >
+    <play-circle-icon />
+    </basic-button>
+</template>
+
+<script>
+import PlayCircleIcon from 'vue-material-design-icons/PlayCircle.vue';
+import BasicButton from './BasicButton.vue';
+
+export default {
+  components: { PlayCircleIcon, BasicButton },
+  props: {
+    image: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
