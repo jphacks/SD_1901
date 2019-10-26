9969039e6b0633f537361937a2fd636fb2ef7945
ShujiOba <shuji365630@gmail.com>
2019-10-26 17:16:27 +0900

    Add storage component

---
+++ frontend/src/components/Storage.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    items: Array,
+  },
+  render(createElement) {
+    return createElement(
+      'div',
+      { class: 'grid' },
+      this.items.map(func => func(createElement)),
+    );
+  },
+};
+</script>
+
+<style scoped>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(170px, 1fr));
+    grid-gap: 100px;
+  }
+
+  .grid > * {
+    margin: 0 auto;
+  }
+</style>
