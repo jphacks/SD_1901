06116ac913bb7e2f5f009dd372df8d9a46028669
ShujiOba <shuji365630@gmail.com>
2019-10-26 11:52:58 +0900

    :art: Add icon

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <menu-icon />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
+    MenuIcon,
   },
 };
 </script>
