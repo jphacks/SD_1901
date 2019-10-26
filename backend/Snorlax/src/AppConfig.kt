2c6bb83379df74f262b50fc1edb27dbee3e7ef56
yt8492 <tmymyut2000@gmail.com>
2019-10-26 23:41:07 +0900

    add AppConfig

---
+++ backend/Snorlax/src/AppConfig.kt
@@ -0,0 +1,16 @@
+package com.yt8492
+
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.HoconApplicationConfig
+import io.ktor.util.KtorExperimentalAPI
+
+@KtorExperimentalAPI
+object AppConfig {
+    private val appConfig = HoconApplicationConfig(ConfigFactory.load())
+    val dbUrl = appConfig.property("ktor.db.jdbcUrl").getString()
+    val dbUser = appConfig.property("ktor.db.dbUser").getString()
+    val dbPassword = appConfig.property("ktor.db.dbPassword").getString()
+    val s3Url = appConfig.propertyOrNull("ktor.aws.s3Url")?.getString()
+    val awsAccessKeyId = appConfig.property("ktor.aws.awsAccessKeyId").getString()
+    val awsSecretAccessKey = appConfig.property("ktor.aws.awsSecretAccessKey").getString()
+}
