05de56fea07b01338b868b6e70b038b4a9b2dda3
schktjm <si2.o.mjt@gmail.com>
2019-10-27 04:43:27 +0900

    WIP

---
+++ frontend/src/views/New.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="new">
+        <div>
+            <new-desk-icon />
+            <p>create a new desk</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import NewDeskIcon from '../components/NewDeskIcon.vue';
+
+export default {
+  components: { NewDeskIcon },
+};
+</script>
+
+<style scoped>
+#new {
+    min-height: 100vh;
+    width: 100vw;
+    background-color: var(--color-white);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
