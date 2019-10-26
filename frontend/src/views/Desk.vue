96ee14d336ad5fc598416d214f026f0ea4ae2f55
ShujiOba <shuji365630@gmail.com>
2019-10-27 05:55:21 +0900

    Can Upload

--- frontend/src/views/Desk.vue
+++ frontend/src/views/Desk.vue
@@ -1,9 +1,13 @@
 <template>
-  <desk-template :deskId="deskId" :items="items" />
+  <desk-template
+    @change="upload"
+    :deskId="deskId"
+    :items="items" />
 </template>
 
 <script>
 import DeskTemplate from '../components/DeskTemplate.vue';
+import File from '../js/files';
 import Api from '../js/api';
 
 export default {
@@ -17,6 +21,18 @@ export default {
     return {
       items: [],
     };
+  },
+  methods: {
+    async upload(e) {
+      const formData = File.createFormData(e.dataTransfer.files[0]);
+      try {
+        const res = await Api.uploadFile(this.deskId, formData).then(x => x.json());
+        console.log(res);
+      } catch (err) {
+        // TODO: Errorハンドリング
+        console.log(err);
+      }
+    },
   },
   async created() {
     this.items = await Api.getDesk(this.deskId).then(x => x.json()).then(x => x.item_info_list);
